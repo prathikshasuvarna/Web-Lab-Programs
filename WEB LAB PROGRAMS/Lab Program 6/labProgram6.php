f54ea0a926dd2634ba515edb3e36b0406e930aaa
Prathiksha Suvarna <53205034+prathikshasuvarna@users.noreply.github.com>
2021-01-05 17:34:25 +0530

    Add files via upload

--- WEB LAB PROGRAMS/Lab Program 6/labProgram6.php
+++ WEB LAB PROGRAMS/Lab Program 6/labProgram6.php
@@ -1,3 +1,10 @@
+<!--USN:4MT17IS039 NAME: PRATHIKSHA SUVARNA
+6. Write a PHP program to keep track of the number of visitors visiting the web page and to
+display this count of visitors, with proper headings. -->
+
+
+
+
 <strong style="color:green;background:lightpink;">
 <?php
     $handle = fopen("counter.txt","r");
---
+++ WEB LAB PROGRAMS/Lab Program 8/Lab Program 8a.php
@@ -0,0 +1,55 @@
+<!--USN:4MT17IS039 NAME: PRATHIKSHA SUVARNA 
+8. Write the PHP programs to do the following:
+a. Implement simple calculator operations.
+b. Find the transpose of a matrix.
+c. Multiplication of two matrices.
+d. Addition of two matrices.
+-->
+
+
+
+<html>
+    <head>
+        <style>
+            table {
+                margin:auto;
+                background-color:silver;
+                align:center;
+            }
+            input,p {
+                text-align:right;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post" action="Lab Program 8a.php">
+            <table>
+            <caption><h2>SIMPLE CALCULATOR</h2></caption>
+            <tr>
+                <td>First Number:</td><td><input type="text" name="n1"/></td>
+                <td rowspan="2"><button type="submit" name="submit" value="calculate">Calculate</td></tr>
+            <tr>
+                <td>Second Number:</td><td><input type="text" name="n2"/></td>
+                <td rowspan="2"><input type="reset" name="clear" value="clear"></td>
+            </tr>
+        </form>
+        <?php
+        if(isset($_POST['submit']))
+        {
+            $n1=$_POST['n1'];
+            $n2=$_POST['n2'];
+            if(is_numeric($n1) and is_numeric($n2))
+            {
+                echo "<tr><td> Addition :</td><td><p>".($n1+$n2)."</p></td>";
+                echo "<tr><td> Subtraction :</td><td><p>".($n1-$n2)."</p></td>";
+                echo "<tr><td> Multiplication :</td><td><p>".($n1*$n2)."</p></td>";
+                echo "<tr><td> Division :</td><td><p>".($n1/$n2)."</p></td>";
+                echo "</table>";
+            }
+            else{
+                echo"<script>alert('Enter valid number');</script>";
+            }
+        }
+        ?>
+</body>
+</html>
